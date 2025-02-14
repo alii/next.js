2ac9296664b46b3d943375be1235747d5492d36a
Alistair Smith <hi@alistair.sh>
2025-02-14 23:38:39 +0000

    change

--- packages/next/src/server/app-render/metadata-insertion/create-server-inserted-metadata.tsx
+++ packages/next/src/server/app-render/metadata-insertion/create-server-inserted-metadata.tsx
@@ -3,7 +3,7 @@ import { renderToReadableStream } from 'react-dom/server.edge'
 import {
   ServerInsertedMetadataContext,
   type MetadataResolver,
-} from '../../../shared/lib/server-inserted-metadata.shared-runtime'
+} from '../../../shared/lib/server-inserted-metadata.shared-runtime.js'
 import { renderToString } from '../render-to-string'
 
 export function createServerInsertedMetadata() {
